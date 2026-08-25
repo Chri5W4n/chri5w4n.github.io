18af96e5a2fb535f375a0ea0d2162c2340055f76
Chris <a36171788@gmail.com>
2026-08-25 20:45:16 +0800

    feat(blog): Configure content Collections schema

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
+export const collections = {
+    'blog': defineCollection({
+        loader: glob({
+            pattern: '**/*.{md,mdx}', 
+            base: './src/content/blog' 
+        }),
+        schema : z.object({
+            title: z.string(),
+            date: z.coerce.date(),
+            description: z.string(),
+        }),
+    }),
+};
